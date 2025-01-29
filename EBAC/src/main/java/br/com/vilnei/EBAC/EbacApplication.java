0189e28294b72bcd25e446e9eee07aeef125ebbc
Vilnei <vilneimartins@gmail.com>
2025-01-29 11:45:50 -0300

    Contruindo Projeto em microsserviços

---
+++ EBAC/src/main/java/br/com/vilnei/EBAC/EbacApplication.java
@@ -0,0 +1,13 @@
+package br.com.vilnei.EBAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbacApplication.class, args);
+	}
+
+}
---
+++ EBAC/src/test/java/br/com/vilnei/EBAC/EbacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vilnei.EBAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
