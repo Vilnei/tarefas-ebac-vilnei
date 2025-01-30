17a76544655861ded827d9702f1a82238ef6000f
Vilnei <vilneimartins@gmail.com>
2025-01-30 13:10:26 -0300

    criandp entidade e modelando BD h2

---
+++ EBAC/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, especie, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Lola', 2, 'cachorro' ,'Shih Tzu', current_date, 'Magro', 'Fernanda', 'Pequeno');
