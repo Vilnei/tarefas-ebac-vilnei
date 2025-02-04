eb85fa4c72290d91c5e8e4df02f8e153a1c9c9bf
Vilnei <vilneimartins@gmail.com>
2025-02-04 10:03:05 -0300

    Exercicio sobre filas e mensageria

---
+++ mensageriaExercicioEbac/src/main/java/br/com/vilnei/mensageriaExercicioEbac/MensageriaExercicioEbacApplication.java
@@ -0,0 +1,13 @@
+package br.com.vilnei.mensageriaExercicioEbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaExercicioEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaExercicioEbacApplication.class, args);
+	}
+
+}
---
+++ mensageriaExercicioEbac/src/test/java/br/com/vilnei/mensageriaExercicioEbac/MensageriaExercicioEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vilnei.mensageriaExercicioEbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaExercicioEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
